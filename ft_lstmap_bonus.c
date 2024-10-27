d15be58e77c10b5c50f41a4fb9c35641e1731900
gcxd68 <94600257+gcxd68@users.noreply.github.com>
2024-10-27 13:49:19 +0100

    Reupload complet

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdosch <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 16:54:15 by gdosch            #+#    #+#             */
+/*   Updated: 2024/10/24 16:54:17 by gdosch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_lst;
+	t_list	*new_node;
+	void	*content;
+
+	if (!f || !del)
+		return (NULL);
+	new_lst = NULL;
+	content = NULL;
+	while (lst)
+	{
+		content = f(lst->content);
+		if (!content)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		new_node = ft_lstnew(content);
+		if (!new_node)
+		{
+			del(content);
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
+	}
+	return (new_lst);
+}
