97bcd94a4c10aece148dc0548f7d46da380d7eec
gcxd68 <94600257+gcxd68@users.noreply.github.com>
2024-10-27 15:20:06 +0100

    Full reupload

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -12,37 +12,33 @@
 
 #include "libft.h"
 
-static t_list	*ft_iter(t_list *lst, void *(*f)(void *), void (*del)(void *))
+static t_list	*ft_cl(t_list **new_lst, void *new_content, void (*del)(void *))
+{
+	if (new_content)
+		del(new_content);
+	ft_lstclear(new_lst, del);
+	return (NULL);
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*new_node;
 	void	*new_content;
 
+	if (!f || !del)
+		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
 		new_content = f(lst->content);
 		if (!new_content)
-		{
-			ft_lstclear(&new_lst, del);
-			return (NULL);
-		}
+			return (ft_cl(&new_lst, new_content, del));
 		new_node = ft_lstnew(new_content);
 		if (!new_node)
-		{
-			del(new_content);
-			ft_lstclear(&new_lst, del);
-			return (NULL);
-		}
+			return (ft_cl(&new_lst, new_content, del));
 		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
 	return (new_lst);
-}
-
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	if (!lst || !f || !del)
-		return (NULL);
-	return (ft_iter(lst, f, del));
 }
