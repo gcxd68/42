71592d49ab11a5bb8e680b2a339872491fd9848c
gcxd68 <94600257+gcxd68@users.noreply.github.com>
2024-11-21 16:00:20 +0100

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -103,23 +103,3 @@ char	*get_next_line(int fd)
 	}
 	return (ft_extract_line(&storage));
 }
-
-int	main(void)
-{
-	int		fd;
-	char	*line;
-
-	fd = open("test.txt", O_RDONLY);
-	if (fd == -1)
-	{
-		printf("Erreur d'ouverture du fichier texte");
-		return (1);
-	}
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		printf("%s", line);
-		free(line);
-	}
-	close(fd);
-	return (0);
-}
