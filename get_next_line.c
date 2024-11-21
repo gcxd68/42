3335469e77753850dce8fa7edf827c06371fab1c
gcxd68 <94600257+gcxd68@users.noreply.github.com>
2024-11-21 15:59:15 +0100

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdosch <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 15:15:45 by gdosch            #+#    #+#             */
+/*   Updated: 2024/11/18 15:15:47 by gdosch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char *)s)[i] = '\0';
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*arr;
+
+	if (nmemb != 0 && size > SIZE_MAX / nmemb)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (!arr)
+		return (NULL);
+	ft_bzero(arr, nmemb * size);
+	return (arr);
+}
+
+static char	*ft_free_mem(char **storage)
+{
+	if (storage && *storage)
+	{
+		free (storage);
+		storage = NULL;
+	}
+	return (NULL);
+}
+
+static char	*ft_extract_line(char **storage)
+{
+	char	*line;
+	char	*new_storage;
+	size_t	i;
+
+	if (!*storage || **storage == '\0')
+		return (ft_free_mem(storage));
+	i = 0;
+	while ((*storage)[i] && (*storage)[i] != '\n')
+		i++;
+	new_storage = NULL;
+	if ((*storage)[i] == '\n')
+	{
+		line = ft_substr(*storage, 0, i + 1);
+		new_storage = ft_strdup((*storage) + i + 1);
+		if (!new_storage)
+			return (NULL);
+	}
+	else
+		line = ft_substr(*storage, 0, i);
+	if (!line)
+		return (NULL);
+	free(*storage);
+	*storage = new_storage;
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*storage = NULL;
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*tmp;
+	ssize_t		bytes_read;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (!storage || !ft_strchr(storage, '\n'))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (ft_free_mem(&storage));
+		if (bytes_read == 0)
+			break ;
+		buffer[bytes_read] = '\0';
+		if (!storage)
+			storage = ft_strdup(buffer);
+		else
+		{
+			tmp = ft_strjoin(storage, buffer);
+			free(storage);
+			storage = tmp;
+		}
+	}
+	return (ft_extract_line(&storage));
+}
---
+++ get_next_line.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdosch <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 15:15:56 by gdosch            #+#    #+#             */
+/*   Updated: 2024/11/18 15:15:59 by gdosch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <ctype.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stddef.h>
+# include <stdint.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <strings.h>
+# include <unistd.h>
+# include <bsd/string.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(char *s);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*get_next_line(int fd);
+
+#endif
