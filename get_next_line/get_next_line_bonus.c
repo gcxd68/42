7378d189a0a245d293fc6d870dfc54cddbc32ee0
gcxd68 <94600257+gcxd68@users.noreply.github.com>
2024-11-27 23:39:42 +0100

    Add files via upload

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -44,66 +44,62 @@ static char	*ft_extract_line(char **depot)
 	size_t	i;
 
 	if (!*depot || (*depot)[0] == '\0')
-		return (free(*depot), *depot = NULL, NULL);
+		return (free(*depot), *depot = 0, NULL);
 	i = 0;
 	while ((*depot)[i] && (*depot)[i] != '\n')
 		i++;
-	new_depot = NULL;
+	line = ft_substr(*depot, 0, i + ((*depot)[i] == '\n'));
+	new_depot = 0;
 	if ((*depot)[i] == '\n')
-	{
-		line = ft_substr(*depot, 0, i + 1);
 		new_depot = ft_strdup((*depot) + i + 1);
-	}
-	else
-		line = ft_substr(*depot, 0, i);
 	if (!line || (!new_depot && (*depot)[i] == '\n'))
-		return (free(*depot), *depot = NULL, NULL);
+		return (free(*depot), *depot = 0, NULL);
 	return (free(*depot), *depot = new_depot, line);
 }
 
-static ssize_t	ft_read_from_fd(int fd, char **depot, char **buffer)
+static ssize_t	ft_read_from_fd(int fd, char **depot)
 {
+	char	*buffer;
 	char	*tmp;
 	ssize_t	bytes_read;
 
-	bytes_read = read(fd, *buffer, BUFFER_SIZE);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (free(*depot), *depot = 0, -1);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
 	if (bytes_read == -1)
-		return (free(*buffer), free(*depot), *depot = NULL, -1);
+		return (free(buffer), free(*depot), *depot = 0, -1);
 	if (bytes_read > 0)
 	{
-		(*buffer)[bytes_read] = '\0';
+		(buffer)[bytes_read] = '\0';
 		if (!*depot)
-			*depot = ft_strdup(*buffer);
+			*depot = ft_strdup(buffer);
 		else
 		{
-			tmp = ft_strjoin(*depot, *buffer);
-			if (!tmp)
-				return (free(*buffer), free(*depot), *depot = NULL, -1);
+			tmp = ft_strjoin(*depot, buffer);
 			free(*depot);
+			if (!tmp)
+				return (free(buffer), *depot = 0, -1);
 			*depot = tmp;
 		}
 	}
-	return (bytes_read);
+	return (free(buffer), bytes_read);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*depot[OPEN_MAX];
-	char		*buffer;
 	ssize_t		bytes_read;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
-		return (NULL);
-	buffer = malloc(BUFFER_SIZE + 1);
-	if (!buffer)
-		return (NULL);
+		return (free(depot[fd]), depot[fd] = 0, NULL);
 	while (!depot[fd] || !ft_strchr(depot[fd], '\n'))
 	{
-		bytes_read = ft_read_from_fd(fd, &depot[fd], &buffer);
+		bytes_read = ft_read_from_fd(fd, &depot[fd]);
 		if (bytes_read == -1)
 			return (NULL);
 		if (bytes_read == 0)
 			break ;
 	}
-	return (free(buffer), ft_extract_line(&depot[fd]));
+	return (ft_extract_line(&depot[fd]));
 }
